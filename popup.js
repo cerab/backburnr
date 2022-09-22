e7738bf64f1d552dfe434e380f261e63bfadfc16
Alexander McPhail <mcphail.alex@unknown6c5ab077fe4b.attlocal.net>
2022-09-22 09:54:56 -0500

    a little tweaking this morning: 1) added a customized PNG 2) changed the name of the extension in the manifest 3) made some changes to the popup.js, popup.html files to make divs that are hidden until hover over the buttons.  These can hold the titles so the buttons aren't so overloaded

--- popup.js
+++ popup.js
@@ -3,13 +3,15 @@ const addFolderButton = document.getElementById("addToFolder");
 const randomizeButton = document.getElementById("random");
 const option1Button = document.getElementById("button1");
 const option2Button = document.getElementById("button2");
+const popup1 = document.getElementById("popup1");
+const popup2 = document.getElementById("popup2");
 
 // chrome.storage.sync.get("color", ({ color }) => {
 //   changeColor.style.backgroundColor = color;
 // });
 
 function addToFolder() {
-    return {docTitle: document.head.title, docURL: document.URL}
+    return { docTitle: document.head.title, docURL: document.URL }
 }
 
 
@@ -32,13 +34,32 @@ randomizeButton.addEventListener("click", async () => {
         chrome.bookmarks.getChildren(`${id}`, (data) => {
             const random1 = data[Math.floor(Math.random() * data.length)];
             const random2 = data[Math.floor(Math.random() * data.length)];
-            option1Button.innerText = `${random1.title}`;
+            //option1Button.innerText = `${random1.title}`;
+            //adding in the title added to the popups, then will disply the popup on mouseover
+            popup1.innerText = `${random1.title}`;
+            popup1.style.display = `none`;
             option1Button.onclick = () => window.open(random1.url);
-            option2Button.innerText = `${random2.title}`;
+            //option2Button.innerText = `${random2.title}`;
+            popup2.innerText = `${random2.title}`;
+            popup2.style.display = `none`;
             option2Button.onclick = () => window.open(random2.url);
         });
     });
 });
+
+option1Button.addEventListener('mouseover', () => {
+    popup1.style.display = 'block';
+})
+
+option1Button.addEventListener('mouseout', () => {
+    popup1.style.display = 'none';
+})
+option2Button.addEventListener('mouseover', () => {
+    popup2.style.display = 'block';
+})
+// option2Button.addEventListener('mouseout', () => {
+//     popup2.style.display = 'none';
+// })
 
 
 
